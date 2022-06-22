d53ef0359b61a6ffbc8f85b3801cc76f89793793
chimatec <chimacodepila@yahoo.com>
2022-06-21 23:32:30 -0500

    Added switch mode button

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,41 +1,33 @@
-import { CssBaseline, Typography } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./header";
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const[darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark': 'light'
+const theme = createTheme({
+  palette:{
+    mode: paletteType,
+    background: {
+      default: paletteType === 'light' ? '#eaeaea' :'#121212'
 
-  useEffect(() =>{
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-
-  function addProduct(){
-    setProducts(prevState =>[...prevState,
-      {
-      id: prevState.length + 101,
-      name: 'product'+ (prevState.length +1), 
-      price: (prevState.length * 100) + 100,
-      brand: 'Pilawil Shoes',
-      description: 'Designed and produced by Chima Wilson in Aba',
-      pictureUrl: 'http://picsum.photos/200',
-    
-    }]) 
-
+    }
   }
+})
+function handleThemeChange() {
+  setDarkMode(!darkMode);
 
+}
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Header />
+      <Header darkMode ={darkMode} handleThemeChange ={handleThemeChange}/>
       <Container>
-        <Catalog products ={products} addProduct ={addProduct}/>
+        <Catalog />
       </Container>
     
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/app/layout/header.tsx
+++ client/src/app/layout/header.tsx
@@ -1,12 +1,17 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header(){
+interface Props {
+    darkMode: boolean;
+    handleThemeChange:() => void;
+}
+export default function Header({darkMode, handleThemeChange}:Props){
     return(
         <AppBar position='static' sx={{mb:4}}>
             <Toolbar>
                 <Typography variant = 'h6'>
                     CHIMA SHOE-STORE
                 </Typography>
+                <Switch checked ={darkMode} onChange ={handleThemeChange} />
             </Toolbar>
 
         </AppBar>
