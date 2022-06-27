698b91ed4900dfb5dbb86e466ae60dcb8b42ddb9
chimatec <chimacodepila@yahoo.com>
2022-06-27 18:11:59 -0500

    Added home page, about us page and Contact us page

---
+++ client/src/features/about/AboutUs.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export default function AboutUs(){
+    return(
+        <Typography variant = 'h2'>
+            About Us
+        </Typography>
+    )
+
+}
---
+++ client/src/features/contact/ContactUs.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export default function ContactUs(){
+    return(
+        <Typography variant = 'h2'>
+          Contact Us
+        </Typography>
+    )
+
+}
