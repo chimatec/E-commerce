6622227c49fa30a68d51839a9a624cd80e9727aa
chimatec <chimacodepila@yahoo.com>
2022-07-10 23:19:58 -0500

    Updated View Cart

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import {useEffect, useState } from "react";
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 export default function ProductDetails(){
     const {id} = useParams<{id: string}>();
-    const [product, setProduct] =useState<Product | null>(null);
+    const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
@@ -19,9 +19,42 @@ export default function ProductDetails(){
     if(!product) return <h3>Product not found</h3>
 
     return(
-        <Typography variant = 'h2'>
-            {product.name}
-        </Typography>
+       <Grid container spacing={6}>
+            <Grid item xs={6}> 
+            <img src={product.pictureUrl} alt ={product.name} style={{width: '100%'}} />
+            </Grid>
+            <Grid>
+                <Typography variant="h3">{product.name}</Typography>
+                <Divider sx={{mb: 2}}/>
+                <Typography variant="h4" color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in Stock</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+       </Grid>
     )
 
 }
